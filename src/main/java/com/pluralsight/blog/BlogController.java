2331108cd985020b82c68f9fd038a7e4ca2afb92
John <johnmancuso5@gmail.com>
2021-12-28 23:53:35 -0500

    Adding all Posts to repository, controller, and the home page

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,11 +1,19 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
+
+    @Autowired
+    private PostRepository postRepository;
 
     // Loads the home.html page
     @RequestMapping("/")
@@ -13,6 +21,10 @@ public class BlogController {
         // Similar to map, but used for UI purposes
         // We use the key  in the html and the value is displayed
         modelMap.put("title", "Blog Post 1");
+
+        List<Post> getPosts = postRepository.getAllPosts();
+        modelMap.put("posts", getPosts);
+
         return "home";
     }
 }
