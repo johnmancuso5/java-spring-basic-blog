d22d46a5d7df1660aecbd677b99c0c633f4b4f66
John <johnmancuso5@gmail.com>
2021-12-29 01:06:41 -0500

    Adding method info and comments to html and controller

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -2,10 +2,8 @@ package com.pluralsight.blog;
 
 import com.pluralsight.blog.data.PostRepository;
 import com.pluralsight.blog.model.Post;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,7 +20,11 @@ public class BlogController {
         this.postRepository = postRepository;
     }
 
-    // Loads the home.html page
+    /**
+     * This page will be triggered when we load into the default page of our website website
+     * @param modelMap
+     * @return Returns the home page with all the of Posts
+     */
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
         // Similar to map, but used for UI purposes
@@ -35,8 +37,14 @@ public class BlogController {
         return "home";
     }
 
-    // This RequestMapping will format to the specific image we click
-    // {id} => Means its a path variable
+    /**
+     * postDetails will be called when the user clicks the image of a Post.
+     *      When the user clicks an image, the new page is: th:href="@{'/post/' +${entry.getId()}}"
+     *      This will then trigger this method to be called.
+     * @param id = PathVariable which is added to the url based on the specific POST the user clicked
+     * @param modelMap - Adds the Post Info to the UI for the personal page
+     * @return URL Page with the specific POST of the ID
+     */
     @RequestMapping("/post/{id}")
     public String postDetails(@PathVariable Long id, ModelMap modelMap) {
         Optional<Post> postOptional = Optional.ofNullable(postRepository.findById(id));
