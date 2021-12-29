f6fc5cafbb7c5b76129043166258fe5fffb6f1f2
John <johnmancuso5@gmail.com>
2021-12-28 23:28:22 -0500

    Adding  ReturnMapping to Controller

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,17 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
 
+    // Loads the home.html page
+    @RequestMapping("/")
+    public String listPosts(ModelMap modelMap) {
+        // Similar to map, but used for UI purposes
+        modelMap.put("title", "Blog Post 1");
+        return "home";
+    }
 }
